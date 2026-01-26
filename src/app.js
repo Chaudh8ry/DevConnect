1fc6808fc8c8ea5b6a9da5fb0667f271c4f16d42
Vishal Chaudhary <vishals08122003@gmail.com>
2026-01-27 00:57:12 +0530

    adding Dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -2,28 +2,28 @@
 const express = require("express");
 const app = express();
 const {connectDB} = require("./config/database.js");
-const User = require("./models/user.js")
+const User = require("./models/user.js");
+const { default: mongoose } = require("mongoose");
+
+// express.json is a middlewear provided by express that converts JSON into JS Object
+app.use(express.json()) 
 
 app.post("/signup",async(req,res) => {
-    const userObj = {
-        firstName: "Vishal",
-        lastName: "Chaudhary",
-        emailID: "vishals8172@gmail.com",
-        password: "1243379",
-        age: "22",
-        gender: "Male"
-    }
 
-    // Create a new instance of the User model using the userObj. 
+    // Create a new instance of the User model using the req sent by User in JSON fromat 
     // 'User' here is a Mongoose model that maps to a MongoDB collection. 
-    const user = new User(userObj); 
+    const user = new User(req.body); 
 
-   // Save the new user document into the MongoDB database. 
-   // This is an asynchronous operation, so we use 'await'. 
-   await user.save(); 
-   
-   // Send a response back to the client confirming success. 
-   res.send("user added Successfully");
+    try{
+        // Save the new user document into the MongoDB database. 
+        // This is an asynchronous operation, so we use 'await'. 
+        await user.save(); 
+        
+        // Send a response back to the client confirming success. 
+        res.send("user added Successfully");
+    }catch(err){
+        res.status(400).send("Error saving the user: " + err.message)
+    }
 })
 
 connectDB()
