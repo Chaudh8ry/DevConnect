318c3385218af57f4be68a06d89209ee6478e371
Vishal Chaudhary <vishals08122003@gmail.com>
2026-02-05 15:26:14 +0530

    Added Comments

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -18,16 +18,24 @@ profileRouter.get("/profile/view",userAuth,async (req,res) => {
     } 
 })
 
+// Route handler for editing user profile
 profileRouter.patch("/profile/edit",userAuth,async(req,res) => {
     try{
+        // Validate the incoming request body fields
         if(!vlaidateEditProfileData(req)){
+            // If invalid fields are found, throw an error
             throw new Error("Invalid Edit Request")
         }else{
+            // Get the currently logged-in user from request
             const loggedInUser = req.user
 
+            // Update the logged-in user's fields with the request body values
             Object.keys(req.body).every((key) => (loggedInUser[key] = req.body[key]))
 
+            // Save the updated user profile to the database
             await loggedInUser.save()
+            
+            // Send a success response back to the client
             res.send(`${loggedInUser.firstName}, your profile is updated Successfully`)
         }
     }catch(err){
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -10,24 +10,25 @@ const validateSignUpData = (req) => {
         throw new Error("Enter a Strong Password")
     }
 }
-
-const vlaidateEditProfileData = (req) => {
-    const allowedEditFields = [
-        "firstName",
-        "lastName",
-        "age",
-        "skills",
-        "about",
-        "gender",
+// Function to validate if the fields being edited are allowed
+ const vlaidateEditProfileData = (req) => { 
+    
+    // List of fields that a user is permitted to edit 
+    const allowedEditFields = [ 
+        "firstName", 
+        "lastName", 
+        "age", 
+        "skills", 
+        "about", 
+        "gender", 
         "photoUrl"
-    ]
-
-    const isEditAllowed = Object.keys(req.body).every((field) =>
-        allowedEditFields.includes(field)
-    )
-
-    return isEditAllowed 
-}
+    ] 
+    
+    // Check if every field in the request body is part of the allowed list 
+    const isEditAllowed = Object.keys(req.body).every((field) => allowedEditFields.includes(field) ) 
+    
+    // Return true if all fields are valid, false otherwise 
+    return isEditAllowed }
 
 module.exports = {
     validateSignUpData,
